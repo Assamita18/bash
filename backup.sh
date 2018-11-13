cc27698b42095d358e92e910d4f51097e766566d
Emilio Martinez <emartinez@tacitknowledge.com>
2018-11-13 14:21:45 -0500

    cleaning up, ready to execute on target directory

--- backup.sh
+++ backup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir renamedLogs
-mv logs/*.log renamedLogs
-for i in renamedLogs/*.log
-do mv "$i" "$i.bck";done
+for i in *.log;do cp "$i" "renamedLogs/$i.bck";done
--- createfiles.sh
+++ createfiles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+mkdir logs
 for i in `seq -w 1 10`
 do
   touch logs/abc.$i.log
