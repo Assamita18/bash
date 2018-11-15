a6969aa7cc14a6859886c04ff02d333094bae7e2
Emilio Martinez <emartinez@tacitknowledge.com>
2018-11-14 23:38:22 -0500

    added special characters validation

--- passwordcheck.sh
+++ passwordcheck.sh
@@ -3,7 +3,7 @@
 #Check $1 for all criteria.
 
 
-if [[ ${#1} -ge 8 && "$1" == *[A-Z]* && "$1" == *[a-z]* && "$1" == *[0-9]* ]]; then
+if [[ ${#1} -ge 8 && "$1" == *[A-Z]* && "$1" == *[a-z]* && "$1" == *[0-9]* && "$1" == *[-@'#'$%'&'*'='.?]*  ]]; then
   echo "Password complies with requirements"
 else echo "Password does not comply with requirements"
 fi
