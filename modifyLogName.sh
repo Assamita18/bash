1f18276278bb36be8d3592bbb3779df3527366d1
Emilio Martinez <emartinez@tacitknowledge.com>
2018-11-16 14:11:21 -0500

    added epoch conversion

--- modifyLogName.sh
+++ modifyLogName.sh
@@ -3,19 +3,20 @@
 #mkdir renamedLogs2
 
 #Part1 -> separate filename in variables - abc.01.txt - done
-#Part2 -> get modification date. Convert epoch to readable.
-#Part3 -> rename to specs. 2018111201.abc.txt
+#Part2 -> get modification date.
+#Part3 -> rename to specs. -> 2018111308.abc.log
 
 for i in *.log
 do
 extension=$(echo $i|cut -f 3 -d ".")
 order=$(echo $i|cut -f 2 -d ".")
 name=$(echo $i|cut -f 1 -d ".")
-modification=$(stat -f %m abc.09.log)
+modification=$(stat -f %m $i)
+dateis=$(date -r $modification '+%Y%m%d')
+# echo "extension is $extension"
+# echo "modification is $modification"
+# echo $dateis$order.$name.$extension
+cp "$i" "renamedLogs2/$dateis$order.$name.$extension"
+echo "renamed $i as $dateis$order.$name.$extension"
 
-echo "extension is $extension"
-echo "modification is $modification"
-echo $modification$order.$name.$extension
 done
-#modification=stat -f  %m $i
-#cp "$i" "renamedLogs2/$modification$order.$extension";done
