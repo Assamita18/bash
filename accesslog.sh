dbf40da17ee3818b1369bc68ff0eb0b6facf731d
Emilio Martinez <emartinez@tacitknowledge.com>
2018-11-20 14:31:54 -0500

    fixed the while.

--- accesslog.sh
+++ accesslog.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
-interval=180
+interval=$1
 startdate=$(date +%s)
 endate=$(($startdate + $interval ))
 
-oldatime=1
-oldmtime=1
-oldctime=1
+oldatime=$startdate
+oldmtime=$startdate
+oldctime=$startdate
+
+echo "$oldatime $oldmtime $oldctime $interval $startdate $endate"
+
+if [[ $interval -le 0 ]]; then
+  echo "we need an observation interval greater than 0 seconds, try again."
+  exit 0
+fi
 
 echo "We are going to report on changes and accesses every 6 seconds" >> report.txt
-while [[ $endate != $(date +%s) ]]; do
+echo "Today is $(date -r $startdate) " >> report.txt
+while [[ "$endate" > "$(date +%s)" ]]; do
   for i in logs/*.log
   do
   owner=$(stat $i | cut -f 5 -d " ")
   newatime=$(stat -f %a $i)
   newmtime=$(stat -f %m $i)
   newctime=$(stat -f %c $i)
-  echo "elapsed one cycle"
+
 
   if [[ $newatime > $oldatime ]]; then
     echo "File $i belongs to $owner, was accessed on $(date -r $newatime)" >> report.txt
@@ -23,6 +31,7 @@ while [[ $endate != $(date +%s) ]]; do
 
     echo "somebody accessed"
   fi
+
   if [[ $newmtime > $oldmtime ]]; then
     echo "File $i belongs to $owner, was modified on $(date -r $newmtime)" >> report.txt
     oldmtime=$newmtime
@@ -30,18 +39,17 @@ while [[ $endate != $(date +%s) ]]; do
     echo "somebody modified"
   fi
 
+  if [[ $newctime > $oldctime ]]; then
+    echo "File $i belongs to $owner, was created on $(date -r $newctime)" >> report.txt
+    oldctime=$newctime
+
+    echo "somebody created"
+  fi
+done;
+  echo "elapsed one cycle $(date +%s)"
   sleep 6
-done
+done;
 
-
-
-
-
-
-done
-
-#if atime is lower than mtime, then ok.
-#if atime is higher than mtime, then changes were made.
 
 # Log all accesses to the files in /etc during the course of a single day.
 # This information should include the filename, user name, and access time. If any alterations to the files take place, that should be flagged.
