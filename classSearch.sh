2dda953cf66941f399a3029d4125ce93fee2eff9
Emilio Martinez <emartinez@tacitknowledge.com>
2018-11-20 16:26:01 -0500

    fixed folder specification

--- classSearch.sh
+++ classSearch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 classname=$1
-directory=$($2 + "/")
+directory=$2
 
+echo $directory
 for i in $2*.jar
 do
-  result=$(zipinfo -1 $i | grep $1.class)
+  result=$(zipinfo -1 $i | grep -w $1.class)
   if [[ -z $result ]]; then
     echo "not found in $i"
   else
